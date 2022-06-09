14f825cd2810c52c1b3bad1470854d9d71347b4b
franclobo <fjbl2788@gmail.com>
2022-06-09 13:31:23 -0500

    Replace function with class

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const createDigits = () => {
-  const digits = [];
-  for (let i = 1; i < 10; i += 1) {
-    digits.push(
-      <button type="submit" key={i}>{i}</button>,
-    );
+class createDigits extends React.Component {
+  render() {
+    const digits = [];
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button type="submit" key={i}>{i}</button>,
+      );
+    }
+    return digits;
   }
-  return digits;
-};
+}
 
 export default createDigits;
