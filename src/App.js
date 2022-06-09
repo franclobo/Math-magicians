d47dd9d1d814d1ef3a9500f0b16fe31003096d2a
franclobo <fjbl2788@gmail.com>
2022-06-09 10:08:30 -0500

    Delete unnecesary elements, create components folder, add calculator.js, edit App.js, index.css and index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import createDigits from './components/calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">
+        <span>0</span>
+      </div>
+
+      <div className="operators">
+        <button type="button">*</button>
+        <button type="button">-</button>
+        <button type="button">+</button>
+        <button type="button">=</button>
+      </div>
+
+      <div className="digits">
+        <button type="button">.</button>
+        { createDigits() }
+        <button type="button">%</button>
+        <button type="button">+/-</button>
+        <button type="button">AC</button>
+      </div>
     </div>
   );
 }
