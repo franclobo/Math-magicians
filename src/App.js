786eeab0891fc2f741c7126e154c6649c4c839c5
franclobo <fjbl2788@gmail.com>
2022-06-09 11:20:43 -0500

    Update styles

--- src/App.js
+++ src/App.js
@@ -8,19 +8,27 @@ function App() {
         <span>0</span>
       </div>
 
+      <div className="transform">
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+      </div>
+
+      <div className="digits">
+        { createDigits() }
+      </div>
+
+      <div className="zero_digits">
+        <button type="button" className="zero">0</button>
+        <button type="button" className="point">.</button>
+      </div>
+
       <div className="operators">
+        <button type="button">&#xF7;</button>
         <button type="button">*</button>
         <button type="button">-</button>
         <button type="button">+</button>
         <button type="button">=</button>
-      </div>
-
-      <div className="digits">
-        <button type="button">.</button>
-        { createDigits() }
-        <button type="button">%</button>
-        <button type="button">+/-</button>
-        <button type="button">AC</button>
       </div>
     </div>
   );
