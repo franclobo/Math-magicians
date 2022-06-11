6540129f4c591e81f90b39b1ffdfa78cc36a0c3b
franclobo <fjbl2788@gmail.com>
2022-06-11 03:00:18 -0500

    Update file extension for Calculator.js in App.js.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Calculator from './components/Calculator.js';
+import Calculator from './components/Calculator';
 
 function App() {
   return <Calculator previous="" operand="" current="0" solved={false} />;
