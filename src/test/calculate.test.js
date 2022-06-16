93d4b964842a4b268e520e5ae33a49b2d7355f7a
franclobo <fjbl2788@gmail.com>
2022-06-16 10:12:15 -0500

    Fix linters errors

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -12,7 +12,7 @@ it('All Clear', () => {
     total: null,
     next: null,
     operation: null,
-  })
+  });
 });
 
 it('Is zero number', () => {
@@ -26,7 +26,7 @@ it('Is zero number', () => {
   expect(result).toStrictEqual({
     total: null,
     next: '0',
-  })
+  });
 });
 
 it('Is number and there is an operation', () => {
@@ -40,8 +40,8 @@ it('Is number and there is an operation', () => {
   expect(result).toStrictEqual({
     total: null,
     next: '32',
-    operation: '+'
-  })
+    operation: '+',
+  });
 });
 
 it('Is number without operation', () => {
@@ -55,7 +55,7 @@ it('Is number without operation', () => {
   expect(result).toStrictEqual({
     total: null,
     next: '20',
-  })
+  });
 });
 
 it('Is number different than zero', () => {
@@ -69,7 +69,7 @@ it('Is number different than zero', () => {
   expect(result).toStrictEqual({
     total: null,
     next: '3',
-  })
+  });
 });
 
 it('Number includes .', () => {
@@ -84,7 +84,7 @@ it('Number includes .', () => {
     total: null,
     next: '2.',
     operation: null,
-  })
+  });
 });
 
 it('Object operation and .', () => {
@@ -99,7 +99,7 @@ it('Object operation and .', () => {
     total: null,
     next: '0.',
     operation: '+',
-  })
+  });
 });
 
 it('Total includes .', () => {
@@ -110,7 +110,7 @@ it('Total includes .', () => {
   };
   const buttonName = '.';
   const result = calculate(obj, buttonName);
-  expect(result).toStrictEqual({})
+  expect(result).toStrictEqual({});
 });
 
 it('Equal', () => {
@@ -125,7 +125,7 @@ it('Equal', () => {
     total: '144',
     next: null,
     operation: null,
-  })
+  });
 });
 
 it('Invert +/-', () => {
@@ -137,10 +137,10 @@ it('Invert +/-', () => {
   const buttonName = '+/-';
   const result = calculate(obj, buttonName);
   expect(result).toStrictEqual({
-    total:  null,
+    total: null,
     next: '-12',
     operation: null,
-  })
+  });
 });
 
 it('Invert -/+', () => {
@@ -155,7 +155,7 @@ it('Invert -/+', () => {
     total: null,
     next: '12',
     operation: null,
-  })
+  });
 });
 
 it("User pressed an operation after pressing '='", () => {
@@ -170,5 +170,5 @@ it("User pressed an operation after pressing '='", () => {
     total: '-12',
     next: null,
     operation: null,
-  })
+  });
 });
